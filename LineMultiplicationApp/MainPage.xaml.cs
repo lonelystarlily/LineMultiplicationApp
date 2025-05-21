45e9ce8760de5602c65c55ff5fd4ba6ed73e8785
lonelystarlily <jungkai17@gmail.com>
2025-05-21 23:36:13 +0900

    화면 이동, 축소, 확대

--- LineMultiplicationApp/MainPage.xaml.cs
+++ LineMultiplicationApp/MainPage.xaml.cs
@@ -1,5 +1,15 @@
 ﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
+using System;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System.Numerics;
+using Microsoft.Maui.Controls.Internals;
+using Microsoft.Maui.Platform;
+using System.Windows;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace LineMultiplicationApp;
 
@@ -19,7 +29,7 @@ public partial class MainPage : ContentPage
             int digits2 = num2.ToString().Length;
             int totalDigits = digits1 + digits2;
 
-            float spacing = 60;
+            float spacing = 120;
             int lineLengthFactor;
             if (totalDigits <= 3)
                 lineLengthFactor = 7;
@@ -45,5 +55,50 @@ public partial class MainPage : ContentPage
             LineCanvas.Drawable = drawable;
             LineCanvas.Invalidate();
         }
+    }
+
+    double translateX = 0, translateY = 0;
+    float scale = 1.0f;
+
+    private void OnZoomInClicked(object sender, EventArgs e)
+    {
+        scale = MathF.Min(3.0f, scale + 0.1f);
+        LineCanvas.Scale = scale;
+        LineCanvas.Invalidate();
+    }
+
+    private void OnZoomOutClicked(object sender, EventArgs e)
+    {
+        scale = MathF.Max(0.5f, scale - 0.1f);
+        LineCanvas.Scale = scale;
+        LineCanvas.Invalidate();
+    }
+
+    double panX = 0, panY = 0;
+
+    void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+
+        switch (e.StatusType)
+        {
+            case GestureStatus.Started:
+                panX = translateX;
+                panY = translateY;
+                break;
+
+            case GestureStatus.Running:
+                translateX = panX + e.TotalX;
+                translateY = panY + e.TotalY;
+                Redraw();
+                break;
+        }
+
+    }
+    void Redraw()
+    {
+        LineCanvas.Scale = scale;
+        LineCanvas.TranslationX = translateX;
+        LineCanvas.TranslationY = translateY;
+        LineCanvas.Invalidate();
     }
 }
